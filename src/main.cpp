6215b510679ff76c413da4b764e0ec45703b36a2
yiqingxiao <xiaoyiqing90@gmail.com>
2025-04-09 14:39:57 -0400

    Resource directory created and whose address is given to the main program via preprocessor variable.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,42 @@
+// Preprocessor variable created when running CMAKE process. This variable 
+// indicates the location where non-compilabe or non c/c++ support files are located.
+#ifndef RES_PATH_XYQ
+#   define RES_PATH_XYQ ""
+#else
+#endif
 
-
-#include <iostream> 
-#include <vector>   
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>   
+#include <vector>  
+
 
 #include "numUtils.h"
 
 
 using namespace std;
 
+// Global string variable holding the full path of the extra resource directory.
+string RES_PATH_XYQ_str = string( RES_PATH_XYQ );
+
+
 int main() {
 
-    // Initialization section
-    // Example: Initialize variables, containers, etc.
     
-    cout << "Hello World!" << endl;
-
+    cout << RES_PATH_XYQ_str << endl;
 
     vector<int> tmp = randIntVectGen( 0, 5, 7 );
+
+    string filename = RES_PATH_XYQ_str + "/Slink_a=100um_b=400um.s2p";
+    cout << "File name: " << filename << endl;
+
+    std::ifstream inputFile( filename );
+    if( !inputFile ){
+        cout << "Failed to read file." << endl;
+    }else{
+        cout << "File read successful." << endl;
+    }
 
     return 0; // Indicates successful completion of the program
 
