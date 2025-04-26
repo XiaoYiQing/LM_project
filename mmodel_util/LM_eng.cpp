b2ca4d8e59786a61dbdb858cfb6fc6a96073b2bc
yiqingxiao <xiaoyiqing90@gmail.com>
2025-04-26 13:43:17 -0400

    First Loewner Matrix construction function complete (untested).

--- mmodel_util/LM_eng.cpp
+++ mmodel_util/LM_eng.cpp
@@ -22,25 +22,48 @@ Eigen::MatrixXd LM_UTIL::build_LM( fData f1Data, fData f2Data ){
     unsigned int col_cnt = f1Size*in_cnt;
 
     // Initialize the Loewner Matrix.
-    Eigen::MatrixXd LM = Eigen::MatrixXd( row_cnt, col_cnt );
+    Eigen::MatrixXcd LM = Eigen::MatrixXcd( row_cnt, col_cnt );
     // Initialize temporary matrix representing the current sub-block being computed.
-    Eigen::MatrixXd currBlk = Eigen::MatrixXd( out_cnt, in_cnt );
+    Eigen::MatrixXcd LM_ij = Eigen::MatrixXcd( out_cnt, in_cnt );
+    // Initialize current freq. data from each partition.
+    Eigen::MatrixXcd f1_D_j = Eigen::MatrixXcd( out_cnt, in_cnt );
+    Eigen::MatrixXcd f2_D_i = Eigen::MatrixXcd( out_cnt, in_cnt );
+    // Initialize current frequencies from each partition.
+    complex<double> f1_j = 0, f2_i = 0;
+    
 
     // Initialize current lead coordinate where the sub-block is to be inserted in the
     // Loewner Matrix.
     unsigned int lead_x = 0, lead_y = 0;
 
+    // Build the Loewner Matrix block by block.
     for( unsigned int i = 0; i < f2Size; i++ ){
-        for( unsigned int j = 0; j < f2Size; j++ ){
 
+        // Obtain the current f2 freq. and data.
+        f2_i = f2Data.get_cplx_f_at( i );
+        f2_D_i = f2Data.get_cplxData_at_f( i );
+
+        for( unsigned int j = 0; j < f1Size; j++ ){
+
+            // Obtain the current f1 freq. and data.
+            f1_j = f1Data.get_cplx_f_at( j );
+            f1_D_j = f1Data.get_cplxData_at_f( j );
+
+            // Obtain the matrix coordinate of the upper-left leading point of the current
+            // LM sub-block being computed.
             lead_x = i*row_cnt;
             lead_y = j*col_cnt;
 
-            
+            // Compute the current Loewner Matrix block.
+            LM_ij = ( f2_D_i - f1_D_j )/( f2_i - f1_j );
+
+            // Insert the current calculated block into its part in the full matrix.
+            LM.block( lead_x, lead_y, out_cnt, in_cnt) = LM_ij;
 
         }
+
     }
 
     return LM;
-    
+
 }
