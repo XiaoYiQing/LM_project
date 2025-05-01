0d5197d0a8a494e172fe7c3fada35cd2a5954576
yiqingxiao <xiaoyiqing90@gmail.com>
2025-04-30 22:47:16 -0400

    Created test for build_LM. Test randomly select a sub-block and checks if it equals expected formula answer. Test successful.

--- tests/tests_LM_eng.cpp
+++ tests/tests_LM_eng.cpp
@@ -6,40 +6,70 @@ using namespace std;
 
 void tests::LM_eng_test_1( unsigned int test_idx ){
 
+    int case_cnt = 0;
 
-    // Define our frequency data object.
-    fData myF;
+    // 0- Base file read check.
+    if( test_idx == case_cnt ){
 
-    // Define the full file name.
-    string fullFileName = RES_PATH_XYQ_str + "/Slink_a=100um_b=400um.s2p";
-    fData::read_sXp_file( myF, fullFileName );
+        // Define our frequency data object.
+        fData myF;
 
-    // Switch the data format into real + imaginary format.
-    myF.data_format_Switch( fData::FDATA_FORMAT::RI );
+        // Define the full file name.
+        string fullFileName = RES_PATH_XYQ_str + "/Slink_a=100um_b=400um.s2p";
+        fData::read_sXp_file( myF, fullFileName );
 
-    // Create a subset linear index array.
-    vector< unsigned int > fr_idx_arr = utils::gen_lin_idx_arr( 0, myF.get_f_cnt() - 1, 100 );
-    // Create a fData subset.
-    shared_ptr<fData> myFr = myF.red_partit( fr_idx_arr );
+        // Switch the data format into real + imaginary format.
+        myF.data_format_Switch( fData::FDATA_FORMAT::RI );
+        myF.data_prefix_switch( fData::METRIC_PREFIX::G );
 
-    // Generate two partitions from this data subset.
-    vector< shared_ptr<fData> > myPartits = myFr->gen_2_partit();
+        // Create a subset linear index array.
+        vector< unsigned int > fr_idx_arr = utils::gen_lin_idx_arr( 0, myF.get_f_cnt() - 1, 100 );
+        // Create a fData subset.
+        shared_ptr<fData> myFr = myF.red_partit( fr_idx_arr );
 
-
-    shared_ptr<fData> partit1 = myPartits.at(0);
-    shared_ptr<fData> partit2 = myPartits.at(1);
-    // Constrcut the Loewner Matrix using the two partitions.
-    Eigen::MatrixXcd myLM = LM_UTIL::build_LM( *partit1, *partit2 );
+        // Generate two partitions from this data subset.
+        vector< shared_ptr<fData> > myPartits = myFr->gen_2_partit();
 
 
-    // Compute the SVD
-    Eigen::JacobiSVD<Eigen::MatrixXcd> mySVD(myLM, Eigen::ComputeThinU | Eigen::ComputeThinV);
+        shared_ptr<fData> partit1 = myPartits.at(0);
+        shared_ptr<fData> partit2 = myPartits.at(1);
+        // Constrcut the Loewner Matrix using the two partitions.
+        Eigen::MatrixXcd myLM = LM_UTIL::build_LM( *partit1, *partit2 );
 
-    // Get the singular values
-    Eigen::VectorXd singularValues = mySVD.singularValues();
+        unsigned int out_cnt = myFr->get_out_cnt();
+        unsigned int in_cnt = myFr->get_in_cnt();
 
-    cout << singularValues << endl;
+        bool match_bool = true;
+        // Random test point selection.
+        unsigned int test_j = randIntGen( 0, partit1->get_f_cnt() - 1, 1 )->at(0);
+        unsigned int test_i = randIntGen( 0, partit2->get_f_cnt() - 1, 1 )->at(0); 
+        Eigen::MatrixXcd test_S_j = partit1->get_cplxData_at_f( test_j );
+        Eigen::MatrixXcd test_S_i = partit2->get_cplxData_at_f( test_i );
+        complex<double> test_f_j = partit1->get_cplx_f_at( test_j );
+        complex<double> test_f_i = partit2->get_cplx_f_at( test_i );
 
-    int lol = 0;
+        // Compute the current reference LM sub-block.
+        Eigen::MatrixXcd test_LM_ij = ( test_S_i - test_S_j )/( test_f_i - test_f_j );
+        // Obtain the generated LM sub-block.
+        Eigen::MatrixXcd LM_ij = 
+            myLM.block( ( test_i )*out_cnt, ( test_j )*in_cnt, out_cnt, in_cnt );
+        // Compute difference between the two LM sub-blocks.
+        Eigen::MatrixXcd LM_ij_diff = test_LM_ij - LM_ij;
+
+        match_bool = match_bool && ( LM_ij_diff.cwiseAbs().maxCoeff() < 1e-9 );
+
+        cout << "Random LM sub-block (" << test_i << ", " << test_j << ") match: " 
+            << match_bool << endl;
+
+        // // Compute the SVD
+        // Eigen::JacobiSVD<Eigen::MatrixXcd> mySVD( myLM, Eigen::ComputeThinU | Eigen::ComputeThinV );
+        // // Get the singular values
+        // Eigen::VectorXd singularValues = mySVD.singularValues();
+        // cout << singularValues << endl;
+
+    }
+
+
+    case_cnt++;
 
 }
