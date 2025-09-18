026700bce8343b13a57f5a3d052b1fcc35e655e8
yiqingxiao <xiaoyiqing90@gmail.com>
2025-09-18 16:36:14 -0400

    eigenUtils: created SVD_econ class for performing econ SVD.

--- utils_XYQ/eigenUtils.h
+++ utils_XYQ/eigenUtils.h
@@ -105,6 +105,39 @@ namespace utils{
     */
     Eigen::MatrixXd gen_orth_basis( Eigen::MatrixXd tarMat );
 
+
+    class SVD_econ{
+
+        public:
+
+            SVD_econ(){
+
+                this->U = Eigen::MatrixXd();
+                this->S = Eigen::MatrixXd();
+                this->V = Eigen::MatrixXd();
+
+            }
+
+            SVD_econ( Eigen::MatrixXd tarMat ){
+                
+                unsigned int k = min( tarMat.rows(), tarMat.cols() );
+
+                Eigen::JacobiSVD<Eigen::MatrixXd> svd( tarMat, Eigen::ComputeThinU | Eigen::ComputeThinV );
+
+                // Extract truncated components
+                this->U = svd.matrixU().leftCols(k);
+                this->S = svd.singularValues().head(k);
+                this->V = svd.matrixV().leftCols(k);
+
+            }
+
+        Eigen::MatrixXd U;
+        Eigen::VectorXd S;
+        Eigen::MatrixXd V;
+
+    };
+
+
 // ====================================================================== <<<<<
 
 }
